b6b40ec257556ee16945bdfb2e362b676b2d6289
KimPhucNguyen <phamthibong1503@gmail.com>
2022-05-24 15:59:50 +0700

    Add Web Api

--- CeleritySolution.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace CeleritySolution.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ CeleritySolution.ViewModels/Catalog/Distributors/DistributorCreateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CeleritySolution.ViewModels.Catalog.Distributors
+{
+    public class DistributorCreateRequest
+    {
+        public string DistributorName { get; set; }
+    }
+}
